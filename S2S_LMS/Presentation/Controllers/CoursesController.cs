52196456c9d692766635d60fe111937ec80533a5
Arseniy Koveshnikov <ars.kvsh@yandex.ru>
2021-06-02 14:11:03 +0300

    first edits

---
+++ S2S_LMS/Presentation/Controllers/CoursesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using S2S_LMS.Presentation.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace S2S_LMS.Presentation.Controllers
+{
+    [ApiController]
+    [Route(template:"courses")]
+    public class CoursesController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok(new CourseModel[]
+            {
+                new CourseModel(){ Name="Компьютерная графика в САПР",Teacher="Аристов А.О.",Description="Есть курсач, по всем темам коллоквиумы, оценка ставится автоматом" },
+                new CourseModel(){ Name="ЛПО САПР",Teacher="Зорин И.А.",Description="Есть неофициальный курсач, автомат, если сделать межсервисную авторизацию" },
+            });
+        }
+    }
+}
---
+++ S2S_LMS/Presentation/Models/CourseModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace S2S_LMS.Presentation.Models
+{
+    public class CourseModel
+    {
+        public string Name { get; set; }
+        public string Teacher { get; set; }
+        public string Description { get; set; }
+    }
+}
