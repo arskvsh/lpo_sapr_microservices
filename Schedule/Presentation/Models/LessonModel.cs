a440782f1385a8f207a3e183cef622af976df47f
arskvsh <ars.kvsh@yandex.ru>
2021-06-12 04:20:11 +0300

    teachers cancelled, schedule wip

---
+++ Schedule/Presentation/Models/LessonModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Schedule.Presentation.Models
+{
+    public class LessonModel
+    {
+        public int Number { get; set; }
+        public string TeacherName { get; set; }
+        public string Room { get; set; }
+    }
+}
--- Schedule/Program.cs
+++ Schedule/Program.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Teachers
+namespace Schedule
 {
     public class Program
     {
